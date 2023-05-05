c9a6cc23f05e953abf11c7352c98a02338b8bb4b
Kwangmo Nam <kwngmo@gmail.com>
2023-05-06 00:53:53 +0900

    minor update on main_views

--- config/production.py
+++ config/production.py
@@ -1,16 +1,14 @@
 from logging.config import dictConfig
-from config.default import *
 from dotenv import load_dotenv
+from config.default import *
 
 load_dotenv(os.path.join(BASE_DIR, '.env'))
-
 SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://{user}:{pw}@{url}/{db}'.format(
     user=os.getenv('DB_USER'),
     pw=os.getenv('DB_PASSWORD'),
     url=os.getenv('DB_HOST'),
     db=os.getenv('DB_NAME')
 )
-# 'sqlite:///{}'.format(os.path.join(BASE_DIR, 'pybo.db'))
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SECRET_KEY = b'!_Uv\x9f^\x82\xde\xbf\xe4\xbc\xa7\xac\xa2\xbe\xa5'
 
--- pybo/views/main_views.py
+++ pybo/views/main_views.py
@@ -5,11 +5,6 @@ from werkzeug.utils import redirect
 bp = Blueprint('main', __name__, url_prefix='/')
 
 
-@bp.route('/hello')
-def hello_pybo():
-    return 'Hello, Pybo!'
-
-
 @bp.route('/')
 def index():
     current_app.logger.info("INFO 레벨로 출력")
